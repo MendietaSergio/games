c8594df2596f1265562de8079723f7279c117e5e
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-05-24 00:06:06 -0300

    añade componente asked, y elimina estilo  app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import { Asked } from './Pages/Asked'
 
 function App() {
   return (
     <div className="App">
-
+      <Asked />
     </div>
   )
 }
