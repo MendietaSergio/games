8b9c5ff89f80e8c2a6c655a83b398962b59668df
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-05-28 12:58:16 -0300

    Añade props, se mejora para poder ser reutilizable

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React from "react";
 
-export const Button = ({
+export const Button = ({ 
     className,
     answer,
     pressBtn,
-    disabled
+    disabled,
+    asked,
+    memo,
+    reset,
+    text
 }) => {
+  if (asked) {
     return (
-        <>
-            <button
-                className={className}
-                onClick={(e) => pressBtn(e)}
-                value={answer}
-                id={`${answer}`}
-                disabled={disabled}
-            >{answer}</button>
-
-        </>
-    )
-}
+      <>
+        <button
+          className={className}
+          onClick={(e) => pressBtn(e)}
+          value={answer}
+          id={`${answer}`}
+          disabled={disabled}
+        >
+          {answer}
+        </button>
+      </>
+    );
+  }
+  if(memo){
+      return(
+        <button
+        className={className}
+        onClick={() => reset()}
+      >
+        {text}
+      </button>
+      )
+  }
+};
