c96798160e2409f4bc6b51247c5b57ddba129b30
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-07 20:09:17 -0300

    Se añaden nuevas img, se sacan las anteriones

--- src/Utils/Images.js
+++ src/Utils/Images.js
@@ -1,40 +1,61 @@
-import meme01 from "../Img/Memo/memes/01.jpg";
-import meme02 from "../Img/Memo/memes/02.jpg";
-import meme03 from "../Img/Memo/memes/03.jpg";
-import meme04 from "../Img/Memo/memes/04.jpg";
-import meme05 from "../Img/Memo/memes/05.jpg";
-import meme06 from "../Img/Memo/memes/06.jpg";
-import meme07 from "../Img/Memo/memes/07.jpg";
-import meme08 from "../Img/Memo/memes/08.jpg";
-import meme09 from "../Img/Memo/memes/09.jpg";
-import meme10 from "../Img/Memo/memes/10.jpg";
-import meme11 from "../Img/Memo/memes/11.jpg";
-import meme12 from "../Img/Memo/memes/12.jpg";
-// import meme13 from "../Img/Memo/memes/13.jpg";
-// import meme14 from "../Img/Memo/memes/14.jpg";
-// import meme15 from "../Img/Memo/memes/15.jpg";
-// import meme16 from "../Img/Memo/memes/16.jpg";
-// import meme17 from "../Img/Memo/memes/17.jpg";
-// import meme18 from "../Img/Memo/memes/18.jpg";
+import src1 from '../Img/Memo/memes/01.jpg';
+import src2 from '../Img/Memo/memes/02.jpg';
+import src3 from '../Img/Memo/memes/03.jpg';
+import src4 from '../Img/Memo/memes/04.jpg';
+import src5 from '../Img/Memo/memes/05.jpg';
+import src6 from '../Img/Memo/memes/06.jpg';
 
-const Images = [
-  meme01,
-  meme02,
-  meme03,
-  meme04,
-//   meme05,
-//   meme06,
-//   meme07,
-//   meme08,
-//   meme09,
-//   meme10,
-//   meme11,
-//   meme12,
-//   meme13,
-//   meme14,
-//   meme15,
-//   meme16,
-//   meme17,
-//   meme18,
+
+const images = [
+  {
+    src: src1,
+    player: '01'
+  },
+  {
+    src: src1,
+    player: '01'
+  },
+  {
+    src: src2,
+    player: '02'
+  },
+  {
+    src: src2,
+    player: '02'
+  },
+  {
+    src: src3,
+    player: '03'
+  },
+  {
+    src: src3,
+    player: '03'
+  },
+  {
+    src: src4,
+    player: '04'
+  },
+  {
+    src: src4,
+    player: '04'
+  },
+  {
+    src: src5,
+    player: '05'
+  },
+  {
+    src: src5,
+    player: '05'
+  },
+  {
+    src: src6,
+    player: '06'
+  },
+  {
+    src: src6,
+    player: '06'
+  },
+
 ];
-export default Images;
+
+export { images } 
