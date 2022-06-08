f38b043f39098100d8464be6ddba0ac83b9f8156
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-07 22:27:09 -0300

    Añade componentes para el enrutamiento, navegacion con react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import { Asked } from './Pages/Asked'
-import { Memo } from './Pages/Memo'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes as Switch } from "react-router-dom";
+import { Header } from "./Components/Header/Header";
+import { Asked } from "./Pages/Asked";
+import { Memo } from "./Pages/Memo";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Asked /> */}
-      <Memo />
-    </div>
-  )
+    <Router>
+      <Header />
+      <div className="App">
+        <Switch>
+          <Route exact path="/asked" element={ <Asked/> } />
+          <Route exact path="/memo" element={ <Memo/> } />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
