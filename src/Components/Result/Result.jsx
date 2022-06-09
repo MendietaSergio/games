aede79a205fb4690ae9db8e4f7ebc940a92b1dcd
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-09 00:24:22 -0300

    Añade mejoras en el titulo a mostrar al ganador

--- src/Components/Result/Result.jsx
+++ src/Components/Result/Result.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
 
-export const Result = ({machineChoice, playerChoice}) => {
-    let resultFinal;
+export const Result = ({ machineChoice, playerChoice }) => {
+  let resultFinal;
 
-    if(playerChoice.defeat === machineChoice.choice && playerChoice.choice){
-        resultFinal = <h1>Ganador: Jugador </h1>
-    } else if( machineChoice.defeat === playerChoice.choice && playerChoice.choice){
-        resultFinal = <h1>Ganador: Máquina</h1>
-    } else if(playerChoice.choice === machineChoice.choice && playerChoice.choice){
-        resultFinal = <h1>Empate</h1>
-    }
+  if (playerChoice.defeat === machineChoice.choice && playerChoice.choice) {
+    resultFinal = (
+      <h1>
+        Ganador: <span className="result-title"> Jugador </span>
+      </h1>
+    );
+  } else if (
+    machineChoice.defeat === playerChoice.choice &&
+    playerChoice.choice
+  ) {
+    resultFinal = <h1>
+    Ganador: <span className="result-title"> Jugador </span>
+  </h1>
+  } else if (
+    playerChoice.choice === machineChoice.choice &&
+    playerChoice.choice
+  ) {
+    resultFinal = <h1>Empate</h1>;
+  }
 
-    return (
-        <h1>{resultFinal}</h1>
-    )
-}
+  return <h1 className="result-final">{resultFinal}</h1>;
+};
