089d4f4d6f59ce3d574df1610cbefbdf07fd9ee1
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-08 23:25:42 -0300

    Añade img de las rutas a cada pagina, se agrega estilos a la img

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../css/Header.css";
-
+import stonepaperscissors from "../../Img/rock-paper-scissors/rock-paper-scissors.png";
+import memo from "../../Img/Memo/memo.png";
+import asked from "../../Img/asked.png";
 export const Header = () => {
   return (
     <header className="container-header">
-      <h1><Link className="home-header" to="/">Games</Link></h1>
+      <h1>
+        <Link className="home-header" to="/">
+          Games
+        </Link>
+      </h1>
       <div className="menu-header">
-        <Link to="/Asked">Asked</Link>
-        <Link to="/Memo">Memo</Link>
+        <Link to="/Asked">
+          <img src={asked} alt="" />
+        </Link>
+        <Link to="/Memo">
+          <img src={memo} alt="" />
+        </Link>
+        <Link to="/stonepaperscissors">
+          <img src={stonepaperscissors} alt="" />
+        </Link>
       </div>
     </header>
   );
