f762a88c1a02a19467ca80321dfaeb2daf2441fb
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2022-06-10 00:10:39 -0300

    Añade pagina home donde muestra title e info

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+        <h3 className="info-home-title">Bienvenidos a la página donde van a encontrar 3 juegos</h3>
+      <div className="info-home">
+        Juegos creados con react para poner en practica los conocimientos de
+        Javascript y React
+      </div>
+    </div>
+  );
+};
