dd1057b3d74e476fe980c598c2a6424d5cf73ea4
Sergio Mendieta <mendieta.sergio.1995@gmail.com>
2023-01-24 10:01:05 -0300

    ADD: component viewMessage page asked

---
+++ src/Components/ViewMessage/ViewMessage.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { getAsked } from "../../Mock/getAsked";
+export const ViewMessage = ({ setCategories, setViewQuestions }) => {
+  const [options, setOptions] = useState({});
+  useEffect(() => {
+    let hash = {};
+    let array = [];
+    array = getAsked.filter((o) =>
+      hash[o.categoria] ? false : (hash[o.categoria] = true)
+    );
+    setOptions(array);
+  }, []);
+  const selectOptions = (categoria) => {
+    setCategories(categoria);
+    setViewQuestions(true);
+  };
+  return (
+    <>
+      {options.length > 0 && (
+        <div className="container-options-categorie">
+          {options.map((categorie, index) => (
+            <button
+              className="btn-categoria"
+              onClick={() => selectOptions(categorie.categoria)}
+              key={index}
+            >
+              {categorie.categoria}
+            </button>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
