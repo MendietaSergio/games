e2e2f48ecabd71d794e93472af3a9c163512c15e
Sergio Mendieta <mendieta.sergio.1995@gmail.com>
2023-01-24 10:02:27 -0300

    UPDATE: view header, styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,28 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../css/Header.css";
-import stonepaperscissors from "../../Img/rock-paper-scissors/rock-paper-scissors.png";
-import memo from "../../Img/Memo/memo.png";
-import asked from "../../Img/asked.png";
+import home from '../../Img/home.png'
 export const Header = () => {
   return (
     <header className="container-header">
-      <h1>
         <Link className="home-header" to="/">
-          Games
+          <img src={home} alt="Principal"  />
         </Link>
-      </h1>
-      <div className="menu-header">
-        <Link to="/Asked">
-          <img src={asked} alt="" />
-        </Link>
-        <Link to="/Memo">
-          <img src={memo} alt="" />
-        </Link>
-        <Link to="/stonepaperscissors">
-          <img src={stonepaperscissors} alt="" />
-        </Link>
-      </div>
     </header>
   );
 };
