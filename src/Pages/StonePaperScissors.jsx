894956cc2f105689f3e4348c8de3502a36d03c0b
Sergio Mendieta <mendieta.sergio.1995@gmail.com>
2023-01-24 15:47:11 -0300

    ADD: view message alert

--- src/Pages/StonePaperScissors.jsx
+++ src/Pages/StonePaperScissors.jsx
@@ -1,30 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { Option } from "../Components/Option/Option";
 import { Result } from "../Components/Result/Result";
-import rock from '../Img/rock-paper-scissors/rock.png'
-import paper from '../Img/rock-paper-scissors/paper.png'
-import scissors from '../Img/rock-paper-scissors/scissors.png'
-import '../css/StonePaperScissors.css'
-import { Attempts } from "../Components/Attempts/Attempts";
+import rock from "../Img/rock-paper-scissors/rock.png";
+import paper from "../Img/rock-paper-scissors/paper.png";
+import scissors from "../Img/rock-paper-scissors/scissors.png";
+import "../css/StonePaperScissors.css";
+import { Message } from "../Components/Message/Message";
+import { Header } from "../Components/Header/Header";
 export const StonePaperScissors = () => {
   const [playerChoice, setPlayerChoice] = useState({});
   const [machineChoice, setMachineChoice] = useState({});
-
+  const [alert, setAlert] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setAlert(false), 5000);
+  }, []);
   const options = [
     {
       choice: "rock",
       defeat: "scissors",
-      img: rock
+      img: rock,
     },
     {
       choice: "paper",
       defeat: "rock",
-      img: paper
+      img: paper,
     },
     {
       choice: "scissors",
       defeat: "paper",
-      img: scissors
+      img: scissors,
     },
   ];
   const selectOptions = (event) => {
@@ -40,23 +44,30 @@ export const StonePaperScissors = () => {
 
   return (
     <div>
+      <Header />
+      {alert && <Message setAlert={setAlert} />}
       <main className="main-info">
         <section>
-          <span className="player">Jugador: <span className="choice">{playerChoice.choice}</span></span>
-          {/* <Attempts player={true} cant={2}/> */}
+          <span className="player">
+            Jugador: <span className="choice">{playerChoice.choice}</span>
+          </span>
         </section>
         <section>
-          <span className="machine">Maquina: <span className="choice">{machineChoice.choice}</span> </span>
-          {/* <Attempts machine={true} cant={2}/> */}
+          <span className="machine">
+            Maquina: <span className="choice">{machineChoice.choice}</span>{" "}
+          </span>
         </section>
       </main>
       <div className="options">
-          {options.map((value,index) =>(
-              <Option key={index} selectOptions={selectOptions} value={options[index]}/>
-          ))}
+        {options.map((value, index) => (
+          <Option
+            key={index}
+            selectOptions={selectOptions}
+            value={options[index]}
+          />
+        ))}
       </div>
       <Result playerChoice={playerChoice} machineChoice={machineChoice} />
-
     </div>
   );
 };
